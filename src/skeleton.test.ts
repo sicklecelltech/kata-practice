e57e6b7b2a5726093a4bae71eeaafa4ad5009dff
Tiffany <twilmotsimpson@gmail.com>
2022-05-26 11:29:01 +0100

    answers

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,18 @@
-import sum from "./skeleton";
+/*
+const destinations = [["e", "n", "e", "e", "n"], ["w", "n", "w", "n", "w", "w", "n"]];
+reachesAnyDestination(["s", "e", "e", "n", "n", "e", "n"], destinations) // should return true
 
-test("sum adds two numbers", () => {
-  expect(sum(4, 3)).toBe(7);
+reachesAnyDestination(["n", "e", "s", "w", "n", "e", "s", "w", "w", "s", "n", "e"], destinations) // should return false
+
+reachesAnyDestination(["n", "s", "n", "n", "e", "n", "w", "w", "s", "w", "w", "w", "n"], destinations) //should return true
+
+reachesAnyDestination(["n", "s"], [["e"]]) //should return false
+reachesAnyDestination(["n", "s"], [[]]) //should return true.
+*/
+
+import reachesAnyDestination from "./skeleton";
+const destinations = [["e", "n", "e", "e", "n"], ["w", "n", "w", "n", "w", "w", "n"]];
+
+test("reaches any destination", () => {
+  expect(reachesAnyDestination(["s", "e", "e", "n", "n", "e", "n"], destinations)).toBe(true);
 });
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,11 +1,91 @@
 /**
  * Adds together two numbers
- * @param a - the first number to add
- * @param b - the second number to add
- * @returns the total
+ * @param directionArray string [] - the first number to add
+ * @param array off string arrays - the second number to add
+ * @returns boolean
  */
-function sum(a: number, b: number): number {
-  return a + b;
+
+/*
+const destinations = [["e", "n", "e", "e", "n"], ["w", "n", "w", "n", "w", "w", "n"]];
+reachesAnyDestination(["s", "e", "e", "n", "n", "e", "n"], destinations) // should return true
+
+
+let [longitudeDirection, latitudeDirection] = calculate(directionArray)
+let [longitudeDestination1, latitudeDestiantion1] = calculate(destiation1Array)
+let [longitudeDestination2, latitudeDestiantion2] = calculate(destiation2Array)
+
+if is valid (longitudeDirection, latitudeDirection,longitudeDestination1, latitudeDestiantion1)
+return true
+elseif isValid (longitudeDirection, latitudeDirection,longitudeDestination2, latitudeDestiantion2)
+return true
+else return false 
+ENDIF
+
+function isValid (longitudeDirection:number[],latitudeDirection:number[],longitudeDestination:number[],latitudeDestination:number[])
+if longitudeDirection + longitudeDestination = 0| latitudeDirection + latitudeDestination
+return true 
+ENDIF
+
+function calculte(string[],) returns latitudeNumber,longitudeNumber
+
+let longitude =0
+let latitude = 0
+
+
+for each element in the string array 
+then if the elemet = e in the string
+latitude +=1 
+else if emlemt = w 
+latutdue -=1
+else if element=n 
+longitude +=1
+else if element = s
+longitude -= 1
+
+reutrn latitude and longitude.
+
+
+*/
+function reachesAnyDestination(directionArray: string[], destination: (string[])[]): boolean {
+  let [destination1Array, destination2Array]= destination
+  let [longitudeDirection, latitudeDirection] = calculate(directionArray)
+  let [longitudeDestination1, latitudeDestiantion1] = calculate(destination1Array)
+  let [longitudeDestination2, latitudeDestiantion2] = calculate(destination2Array)
+if (isValid(longitudeDirection,longitudeDestination1)&& isValid(latitudeDirection,latitudeDestiantion1)){
+  return true
+}else if (isValid(longitudeDirection,longitudeDestination2)&& isValid(latitudeDirection,latitudeDestiantion2)){
+  return true
+}else {
+  return false
 }
 
-export default sum;
+}
+
+function calculate(array:string[]):number[]{
+  let longitude =0
+  let latitude = 0
+
+  for(let i of array){
+    if (i === 'e'){
+        latitude+= 1
+    }else if (i === 'w'){
+      latitude += -1
+    } else if(i==='n'){
+        longitude += 1
+    }else if ( i === 's'){
+      longitude+= -1
+    }
+    }
+    return [latitude,longitude]
+}
+
+function isValid(a:number, b:number):boolean{
+
+  if (a+b === 0){
+    return true
+  }else {
+    return false
+  }
+}
+
+export default reachesAnyDestination;
